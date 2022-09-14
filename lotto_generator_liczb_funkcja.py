fb09603b4ba6e4df5d6634253aa18d29a87f0ebc
sanczo <wiktor_m@yahoo.com>
2022-09-14 21:41:24 +0200

    funkcja losowania liczb

--- lotto_generator_liczb_funkcja.py
+++ lotto_generator_liczb_funkcja.py
@@ -2,5 +2,24 @@
 # -*- coding: utf-8 -*-
 import random
 
-def lotto_generator(6, 49):
+ilosc_losowan = int(input("Podaj liczbę losowań "))
+def lotto_generator(x, y):
+    x = 6
+    y = 49
+    liczby = []
+    i = 0
+    while i < x:
+        liczba = random.randint(1, y)
+        if liczby.count(liczba) == 0:
+            liczby.append(liczba)
+            i = i + 1
+    return liczby
+
+
+for i in range(1, ilosc_losowan+1):
+    print(f'Typuję 6 z 49 liczb, Losuję {i} raz')
+    print(lotto_generator(6, 49))
+    print()
+
+
 
